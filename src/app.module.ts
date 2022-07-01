d57c737ed1244cfdb6dc0e21dc5c6a2a57250140
NichZilli <nich.zilli@hotmail.com>
2022-06-30 22:30:44 -0300

    Subindo arquivos projeto Nest+TypeScript

---
+++ src/app.module.ts
@@ -0,0 +1,28 @@
+import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { SequelizeModule } from '@nestjs/sequelize/dist/sequelize.module';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { Livro } from './livro.model';
+import { LivrosController } from './livros.controller';
+import { LivrosService } from './livros.service';
+
+@Module({
+  imports: [
+    ConfigModule.forRoot(),
+    SequelizeModule.forRoot({
+      dialect: 'postgres',
+      host: 'localhost',
+      port: 5432,
+      username: process.env.USER_DB,
+      password: process.env.PASS_DB,
+      database: 'livraria',
+      autoLoadModels: true,
+      synchronize: true,
+    }),
+    SequelizeModule.forFeature([Livro])
+  ],
+  controllers: [AppController, LivrosController],
+  providers: [AppService, LivrosService],
+})
+export class AppModule {}
